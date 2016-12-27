2d852a78e4b1f9762e57d4c5d803e3e7eb776c16
Víctor Manuel Rivas Santos <vrivas@ujaen.es>
2016-12-27 17:15:26 +0100

    Re-run finally works

--- isafm2017.main.js
+++ isafm2017.main.js
@@ -418,7 +418,7 @@ d6.sendNewSolution = function (_rbfnn, _tsme, _url) {
 d6.rerun=function( secs ) {
   secs=(typeof secs==="undefined")?10:secs;
   $("#rerun_wrapper").attr( "class", "visible" )
-  if( secs>0 ) {
+  if( secs>=0 ) {
     $("#rerun_seconds").html( secs-- );
     setTimeout( "d6.rerun("+secs+")", 1000 );
   } else {
