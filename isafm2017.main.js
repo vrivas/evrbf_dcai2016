3e82102a139882afb978624fa25a6fa5af5bb13d
Víctor Manuel Rivas Santos <vrivas@ujaen.es>
2016-12-28 16:51:59 +0100

    Changing the number of generations to 10 as in previous paper

--- isafm2017.main.js
+++ isafm2017.main.js
@@ -464,7 +464,7 @@ d6.main = function (maxExecutions) {
                     , "inputDimension": d6.inputDimension
                     , "tournamentSize": 3
                     , "popSize": 15
-                    , "numGenerations": 50
+                    , "numGenerations": 10
                     , "replaceRate": .2
                     , "xOverRate": .2
                     , "mutRate": 0.8
